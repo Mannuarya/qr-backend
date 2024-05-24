8d62e6489910917e9167de6aa2f35c2a93e54053
Mannuarya <mannuarya2002@gmail.com>
2024-05-24 16:02:31 +0530

    4

--- models/User.js
+++ models/User.js
@@ -1,30 +1,31 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    contact: {
-        type: String,
-    },
-    address: {
-        type: String,
-    },
-    profileImage: {
-        type: String, // this field stores the path to the uploaded image
-        default:
-            "https://img.icons8.com/?size=100&id=492ILERveW8G&format=png&color=000000", // this is the default image to display if no image is uploaded
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  profileImage: {
+    type: String, // this field stores the path to the uploaded image
+    default:
+      "https://img.icons8.com/?size=100&id=492ILERveW8G&format=png&color=000000", // this is the default image to display if no image is uploaded
+  },
+  isVerified: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("NewUser", UserSchema);
